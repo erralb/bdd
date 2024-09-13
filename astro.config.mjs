fb90fdfc60c3aaa067597e30550c4fe5eb1e47bc
Pierre Blarre <pierre.blarre@gmail.com>
2024-09-13 11:14:35 +0200

    Titles changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,38 +38,45 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: '1 - Relationnelles & SQL',
-					collapsed: true,
+					// collapsed: true,
 					items: [
 						{
-							label: '1.1 - Concepts',
+							label: '1. Concepts',
 							collapsed: true,
 							autogenerate: {
 								directory: '01-concepts',
 							}
 						},
 						{
-							label: '1.2 - Algèbre Relationnelle',
+							label: '2. Algèbre Relationnelle',
 							collapsed: true,
 							autogenerate: {
 								directory: '02-algebre'
 							}
 						},
 						{
-							label: '1.3 - SQL DML 1',
+							label: '3. Les bases du SQL',
 							collapsed: true,
 							autogenerate: {
 								directory: '03-sql-dml1'
 							}
 						},
 						{
-							label: '1.4 -SQL DML 2',
+							label: '4. SQL Avancé',
 							collapsed: true,
 							autogenerate: {
 								directory: '03-sql-dml2'
 							}
 						},
 						{
-							label: '1.5 - SQL DDL',
+							label: '5. Création de BDD',
+							collapsed: true,
+							autogenerate: {
+								directory: '04-sql-ddl'
+							}
+						},
+						{
+							label: '6. Triggers & Procédures',
 							collapsed: true,
 							autogenerate: {
 								directory: '04-sql-ddl'
@@ -78,48 +85,48 @@ export default defineConfig({
 
 					]
 				},
-				{
-					label: '2 - Conception',
-					collapsed: true,
-					items: [
-						{
-							label: '2.1 - Introduction',
-							collapsed: true,
-							autogenerate: {
-								directory: '05-conception'
-							}
-						},
-						{
-							label: '2.2 - Modèle Conceptuel de Données (MCD)',
-							collapsed: true,
-							autogenerate: {
-								directory: '06-mcd'
-							}
-						},
-						{
-							label: '2.3 - Schéma Conceptuel',
-							collapsed: true,
-							autogenerate: {
-								directory: '07-schema'
-							}
-						},
-						{
-							label: '2.4 - Modèle Logique de Données (MLD)',
-							collapsed: true,
-							autogenerate: {
-								directory: '08-mld'
-							}
-						},
+				// {
+				// 	label: '2 - Conception',
+				// 	collapsed: true,
+				// 	items: [
+				// 		{
+				// 			label: '1. Introduction',
+				// 			collapsed: true,
+				// 			autogenerate: {
+				// 				directory: '05-conception'
+				// 			}
+				// 		},
+				// 		{
+				// 			label: '2. Modèle Conceptuel de Données (MCD)',
+				// 			collapsed: true,
+				// 			autogenerate: {
+				// 				directory: '06-mcd'
+				// 			}
+				// 		},
+				// 		{
+				// 			label: '3. Schéma Conceptuel',
+				// 			collapsed: true,
+				// 			autogenerate: {
+				// 				directory: '07-schema'
+				// 			}
+				// 		},
+				// 		{
+				// 			label: '4. Modèle Logique de Données (MLD)',
+				// 			collapsed: true,
+				// 			autogenerate: {
+				// 				directory: '08-mld'
+				// 			}
+				// 		},
 
-					]
-				},
-				{
-					label: '3 - NoSQL',
-					collapsed: true,
-					autogenerate: {
-						directory: '09-nosql'
-					}
-				}
+				// 	]
+				// },
+				// {
+				// 	label: '3 - NoSQL',
+				// 	collapsed: true,
+				// 	autogenerate: {
+				// 		directory: '09-nosql'
+				// 	}
+				// }
 			]
 		}),
 	],
